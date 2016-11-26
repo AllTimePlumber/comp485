a224bbd4bdfd1e1d83258a0672bfcb6887a76bf7
Eric Demauro <erickdemauro@gmail.com>
2016-11-25 16:42:24 -0800

    added all images (still need to clean this up). Added jQuery and Bootstrap. Moved some JS to a separate file (still needs a lot of cleaning). Added all HTML content (still needs cleaning).

---
+++ quotes.js
@@ -0,0 +1,49 @@
+
+var howMany = 31
+var quote = new Array(howMany+1)
+quote[0]="<i><b>I don't know what science fiction he is reading, said Dr. LaHaye. We believe the Rapture is going to come, not his nonsense that Christ came back in 68 A.D.<br>Dallas Morning News, Apocalyptic Feud"
+quote[1]="<i><b>(Jesus) beheld (Jerusalem), and wept over it, saying, If thou hadst known, even thou, at least in this thy day, the things which belong unto thy peace! But now they are hid from thine eyes. For the days shall come upon thee, that thine enemies shall cast a trench about thee, and compass thee round,     and keep thee in on every side, and shall lay thee even with the ground, and     thy children within thee; and they shall not leave in thee one stone upon  another; because thou knewest not the time of thy visitation.</i></b><br> Luke 19:41-44 (KJV)"
+quote[2]="<i><b>This generation shall not pass, till all be fulfilled.</i></b><br>Jesus, Luke 21:32 (KJV)"
+quote[3]="<i><b>The letter killeth, but the Spirit giveth life.</i></b><br>Paul, Galatians 4:21 (KJV)"
+quote[4]="<i><b>What has the Church to do with the Academy?</i></b><br>Tertullian, <i>De Praescr.</i> 7"
+quote[5]="<i><b>Twenty doctors expound one text twenty ways, and with an antitheme of half an inch some of them draw a thread of nine days long.</i></b><br>William Tyndale, <i>Obedience of a Christian Man</i>"
+quote[6]="<i><b>(The History of Exegesis) has positive truths to teach as well as errors to dispel. It may show us the stagnation which poisons the atmosphere of Theology when Progress is violently arrested, and Freedom authoritatively suppressed.</i></b><br> F.W. Farrar, <i>History of Interpretation</i>, xv"
+quote[7]="<i><b>(The History of Exegesis) may show us above all that the strength of the Church is not to be identified with the continuance of methods which have been tried and found wanting, or with the preservation of systems which have been condemned by the long results of time.</i></b> <br>F.W. Farrar, <i>History of Interpretation, xv</i>"
+quote[8]="<i><b>Truth is compared in Scripture to a streaming fountain ; if her waters flow not in a perpetual progression they sicken into a muddy pool of conformity and tradition.</i></b><br> Milton"
+quote[9]="<i><b>Newton's discoveries are against evident testimonies of Scripture. </i></b><br>Dr. John Owen"
+quote[10]="<i><b>It is the duty of the Exegete to explain, not explain away.</i></b>  <br>F.W. Farrar, <i>History of Interpretation, xxv</i>"
+quote[11]="<i><b>Either Plato philonises, or Philo platonises. </i></b><br>Suidas ; Jer. <i>Catal</i>. 11 ; <i>Ep.</i> 83"
+quote[12]="<i><b>Granting that Christianity is the pure truth, who will answer for it, that the orthodoxy which prevails at any one time is equivalent with pure Christianity?</i></b><br>Chalybaus, <i>History of Special Philosophie</i>, p. 419"
+quote[13]="<i><b>the Fall of Jerusalem and all the events which accompanied and followed it in the Roman world and in the Christian world, had a significance which it is hardly possible to overestimate.</b></i><br> F.W. Farrar, <i>Early Days of Christianity,</i> p. 489"
+quote[14]="<i><b>the Fall of Jerusalem was, in the fullest sense, the Second Advent of the Son of Man which was primarily contemplated by the earliest voices of prophecy</i></b><br> F.W. Farrar, <i>Early Days of Christianity</i>, ii., p. 49"
+quote[15]="<i><b>If Preterism is true, which it emphatically is not, the saints of God would have ceased waiting for God's Son from heaven after A.D. 70. Such a postulate, whether expressly stated or implied, is nothing more than a confused set of tattdemalions..</i></b>Given O. Blakely"
+quote[16]="<i><b>There will be no end to the increase of His government or of peace, On the throne of David and over his kingdom, To establish it and to uphold it with justice and righteousness From then on and forevermore.  The zeal of the Lord of hosts will accomplish this.</i></b>Isaiah 9:7"
+quote[17]="<i><b>In all history there is no drama of more terrible interest than that which terminated with the total destruction of Jerusalem.</b><br>G.A. Henty"
+quote[18]="<i><b>The Revelation which was made by God to John the evangelist in the island of Patmos, into which he was thrown by Nero Caesar. </i></b><br>Syriac Vulgate Bible (AD508)" 
+quote[19]="<i><b>Deliver me... from unhealthy enquiries and interminable disputes.</i></b><br>Prayer of Bishop Andrews" 
+quote[20]="<i><b>The plague of the Church for above a thousand years has been the enlarging our creed, and making more fundamentals than God ever made. </i></b><br>Baxter" 
+quote[21]="<i><b>It is significant that the decline of the Roman Empire dates from the fall of Jerusalem.</i></b><br>G. N. M. Collins"
+quote[22]="<i><b>I do not think that he refers to the resurrection of the body, but to the resurrection of the soul in this life; to the regaining of the image which Adam lost.</i></b><br>Adam Clarke"
+quote[23]="<i><b>It is also observable, that the Romans after having been thus made the executioners of divine vengeance on the Jewish nation, never prospered as they had done before; but the Lord evidently fought against them, and all the nations which composed their overgrown empire; till at last it was subverted, and their fairest cities and provinces were ravaged by barbarous invaders.</i></b><br>Thomas Scott"
+quote[24]="<i><b>We thus learn that the things prepared by God for the coming age, which are for our glory, are spiritual things.  And not only are they spiritual things, but they are communicated by means of spiritual words; and they must be spiritually discerned.</i></b> - Philip Mauro"
+quote[25]="<i><b>But by the destruction of Jerusalem this mountain (of division) shall be made to cleave in the midst, and so the Jewish pale  shall be taken down, and the church laid in common with the Gentiles, who were  made one with the Jews by the breaking down of this middle wall of  partition</i></b><br>Matthew Henry on Ephesians 2:14"
+quote[26]="<i><b>But it pleased God otherwise to dispose of it; for before I had read to the end of the first verse of the book, these words, which must come to pass presently, had such an impression on my mind, offering themselves as a key to the whole prophecie, (in like manner as, this generation shall not passe till all these things be fulfilled, Matt. 24.34. have demonstrated infallibly to what coming of Christ the whole Chapter did belong).</i></b><br>Henry Hammond (1653), Father of English Exegesis"
+quote[27]="<i><b>If forgers and malefactors are put to death by the secular power, there is much more reason for excommunicating and even putting to death one convicted of heresy.</i></b> St. Thomas Aquinas, Summa Theologica"
+quote[28]="<i><b>Now although what is said here by these holy men is orthodox, care must be taken to avoid the reproach which some receive for this.  For the early doctors and saints were so intent upon refuting the emerging errors concerning the faith that they seemed meanwhile to fall into the opposite ones.</i></b><br>St. Thomas Aquinas, Commentary on John"
+quote[29]="<i><b>The destruction of Jerusalem in A.D.70, only five years after our epistle, was the greatest single event of a thousand years, and religiously significant beyond anything else that ever occurred in human history. </b></i>(James Burton Coffman, <i>Commentary on James, 1 &amp; 2 Peter</i>, p. 231)"
+quote[30]="<i><b>Tis evident that when Christ speaks of his coming; his being revealed; his coming in his Kingdom; or his Kingdom’s coming; He has respect to his appearing in those great works of his Power Justice and Grace, which should be in the Destruction of Jerusalem and other extraordinary Providences which should attend it.</i></b> - Jonathan Edwards, Miscellany #1199"
+quote[31]="<i><b>In all history there is no drama of more terrible interest than that which terminated with the total destruction of Jerusalem. - </i></b> G.A. Henty"
+
+function rndnumber() {
+  var randscript = -1
+  while (randscript < 0 || randscript > howMany || isNaN(randscript)){
+    randscript = parseInt(Math.random()*(howMany+1))
+  }
+  return randscript
+}
+
+quo = rndnumber()
+quox = quote[quo]
+$(function() {
+  $('#quoteText').html(quox);
+});
